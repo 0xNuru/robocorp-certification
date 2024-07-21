b8ae92182590405160eb2cd8e943194445e60705
nuru <0xnuru@gmail.com>
2024-07-21 20:30:57 +0100

    refactor: move download_traffic_data logic into the task since it's only going to be called once

--- inhuman-insurance-inc-ais-robot/tasks.py
+++ inhuman-insurance-inc-ais-robot/tasks.py
@@ -5,16 +5,13 @@ http = HTTP()
 
 @task
 def produce_traffic_data():
-    download_traffic_data()
-    print("produce")
-
-@task
-def consume_traffic_data():
-    print("consume")
-
-def download_traffic_data():
     http.download(
         url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
         target_file="output/traffic.json",
         overwrite=True,
     )
+    print("produce")
+
+@task
+def consume_traffic_data():
+    print("consume")
