3d4a24773d6b7b5494ebb8e2ca12793397ccb69e
nuru <0xnuru@gmail.com>
2024-07-23 11:03:33 +0100

    feat: add process traffic data demo fucntion to consume task

--- inhuman-insurance-inc-ais-robot/tasks/consume.py
+++ inhuman-insurance-inc-ais-robot/tasks/consume.py
@@ -1,2 +1,14 @@
+from robocorp import workitems
+
+
 def consume_traffic_data():
-    print("consume")
+    """
+    Inhuman Insurance, Inc. Artificial Intelligence System robot.
+    Consumes traffic data work items.
+    """
+    process_traffic_data()
+
+def process_traffic_data():
+    for item in workitems.inputs:
+        print(item)
+        traffic_data = item.payload
